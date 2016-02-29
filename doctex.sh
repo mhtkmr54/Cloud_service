84ef89809e8a2d487e68be4dd47aa5c9763a016e
mhtkmr54 <mhtkmr54@users.noreply.github.com>
2016-02-29 12:28:34 +0530

    Update Shell scrpt

--- doctex.sh
+++ doctex.sh
@@ -2,6 +2,11 @@
 
 # [arg 1] is input file, complete path
 # [arg 2] is output dir, complete path
+#doc to odt using loffice
+#W2LPATH="/home/username/writer2latex05"
+#set JAVAEXE="/path/to/java/executable/”
+#chmod +x w2l
+#odt to latex require w2l
 
 echo 'input: ' $1
 echo 'out dir: ' $2
